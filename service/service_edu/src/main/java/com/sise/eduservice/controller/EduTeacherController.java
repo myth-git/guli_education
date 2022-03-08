2c8e68c5f32a99367530f54eed3f1717f22d4b20
myth-git <1610680426@qq.com>
2022-03-08 20:24:39 +0800

    讲师模块的查询和删除

--- service/service_edu/src/main/java/com/sise/eduservice/controller/EduTeacherController.java
+++ service/service_edu/src/main/java/com/sise/eduservice/controller/EduTeacherController.java
@@ -1,9 +1,12 @@
 package com.sise.eduservice.controller;
 
 
-import org.springframework.web.bind.annotation.RequestMapping;
+import com.sise.eduservice.entity.EduTeacher;
+import com.sise.eduservice.service.EduTeacherService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
-import org.springframework.web.bind.annotation.RestController;
+import java.util.List;
 
 /**
  * <p>
@@ -14,8 +17,26 @@ import org.springframework.web.bind.annotation.RestController;
  * @since 2022-03-08
  */
 @RestController
-@RequestMapping("/eduservice/edu-teacher")
+@RequestMapping("/eduservice/teacher")
 public class EduTeacherController {
+    //把service注入
+    @Autowired
+    private EduTeacherService teacherService;
+
+    //1 查询讲师表所有数据
+    //rest风格
+    @GetMapping("findAll")
+    public List<EduTeacher> findAllTeacher() {
+        //调用service的方法实现查询所有的操作
+        List<EduTeacher> list = teacherService.list(null);
+        return list;
+    }
+
+    @DeleteMapping("{id}")//采用路径传递 @PathVariable
+    public boolean removeTeacher(@PathVariable String id) {
+        boolean flag = teacherService.removeById(id);
+        return flag;
+    }
 
 }
 
