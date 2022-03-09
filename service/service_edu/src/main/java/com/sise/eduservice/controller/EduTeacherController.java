59d4a46b0cad1623c725a301386e80524ceeb936
myth-git <1610680426@qq.com>
2022-03-09 10:16:13 +0800

    分页查询和条件组合查询

--- service/service_edu/src/main/java/com/sise/eduservice/controller/EduTeacherController.java
+++ service/service_edu/src/main/java/com/sise/eduservice/controller/EduTeacherController.java
@@ -1,13 +1,17 @@
 package com.sise.eduservice.controller;
 
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.sise.commonutils.R;
 import com.sise.eduservice.entity.EduTeacher;
+import com.sise.eduservice.entity.vo.TeacherQuery;
 import com.sise.eduservice.service.EduTeacherService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -48,6 +52,75 @@ public class EduTeacherController {
         }else {
             return R.error();
         }
+    }
+    //3 分页查询讲师的方法
+    //current 当前页
+    //limit 每页记录数
+    @GetMapping("pageTeacher/{current}/{limit}")
+    public R pageListTeacher(@PathVariable long current,
+                             @PathVariable long limit) {
+        //创建page对象
+        Page<EduTeacher> pageTeacher = new Page<>(current,limit);
+
+        //调用方法实现分页
+        //调用方法时候，底层封装，把分页所有数据封装到pageTeacher对象里面
+        teacherService.page(pageTeacher,null);
+
+        long total = pageTeacher.getTotal();//总记录数
+        List<EduTeacher> records = pageTeacher.getRecords(); //数据list集合
+//        第一种方式写法
+//        Map map = new HashMap();
+//        map.put("total",total);
+//        map.put("rows",records);
+//        return R.ok().data(map);
+
+        return R.ok().data("total",total).data("rows",records);
+    }
+
+    /*
+    * @RequestBody:使用json数据封装到对象的对象里面，需要使用post请求提交
+    * @ResponseBody:返回的是json数据
+    * @RequestBody(required = false)：表示的是参数值可以为空
+    * TeacherQuery teacherQuery：条件组合查询封装到对象中，再从对象中调用
+    * */
+    //4 条件查询带分页的方法
+    @PostMapping("pageTeacherCondition/{current}/{limit}")
+    public R pageTeacherCondition(@PathVariable long current,@PathVariable long limit,
+                                  @RequestBody(required = false) TeacherQuery teacherQuery) {
+        //创建page对象
+        Page<EduTeacher> pageTeacher = new Page<>(current,limit);
+
+        //构建条件
+        QueryWrapper<EduTeacher> wrapper = new QueryWrapper<>();
+        // 多条件组合查询
+        // mybatis学过 动态sql
+        String name = teacherQuery.getName();
+        Integer level = teacherQuery.getLevel();
+        String begin = teacherQuery.getBegin();
+        String end = teacherQuery.getEnd();
+        //判断条件值是否为空，如果不为空拼接条件
+        if(!StringUtils.isEmpty(name)) {
+            //构建条件
+            wrapper.like("name",name);
+        }
+        if(!StringUtils.isEmpty(level)) {
+            wrapper.eq("level",level);
+        }
+        if(!StringUtils.isEmpty(begin)) {
+            //大于创建时间，因为写的是范围
+            wrapper.ge("gmt_create",begin);
+        }
+        if(!StringUtils.isEmpty(end)) {
+            //小于创建时间
+            wrapper.le("gmt_create",end);
+        }
+
+        //调用方法实现条件查询分页
+        teacherService.page(pageTeacher,wrapper);
+
+        long total = pageTeacher.getTotal();//总记录数
+        List<EduTeacher> records = pageTeacher.getRecords(); //数据list集合
+        return R.ok().data("total",total).data("rows",records);
     }
 
 }
