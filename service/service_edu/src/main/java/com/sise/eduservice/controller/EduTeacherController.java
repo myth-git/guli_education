994e91ef4b8c05b0cea362eecc4a885c75a8d12d
myth-git <1610680426@qq.com>
2022-03-09 10:38:12 +0800

    添加和修改讲师

--- service/service_edu/src/main/java/com/sise/eduservice/controller/EduTeacherController.java
+++ service/service_edu/src/main/java/com/sise/eduservice/controller/EduTeacherController.java
@@ -24,7 +24,7 @@ import java.util.List;
  * @author myth
  * @since 2022-03-08
  */
-@Api(description="讲师管理")//swagger中文友好提示
+@Api(description = "讲师管理")//swagger中文友好提示
 @RestController
 @RequestMapping("/eduservice/teacher")
 public class EduTeacherController {
@@ -39,20 +39,21 @@ public class EduTeacherController {
     public R findAllTeacher() {
         //调用service的方法实现查询所有的操作
         List<EduTeacher> list = teacherService.list(null);
-        return R.ok().data("items",list);
+        return R.ok().data("items", list);
     }
 
     @ApiOperation(value = "逻辑删除讲师")
     @DeleteMapping("{id}")//采用路径传递 @PathVariable
     public R removeTeacher(@ApiParam(name = "id", value = "讲师ID", required = true)
-                                     @PathVariable String id) {
+                           @PathVariable String id) {
         boolean flag = teacherService.removeById(id);
         if (flag) {
             return R.ok();
-        }else {
+        } else {
             return R.error();
         }
     }
+
     //3 分页查询讲师的方法
     //current 当前页
     //limit 每页记录数
@@ -60,11 +61,11 @@ public class EduTeacherController {
     public R pageListTeacher(@PathVariable long current,
                              @PathVariable long limit) {
         //创建page对象
-        Page<EduTeacher> pageTeacher = new Page<>(current,limit);
+        Page<EduTeacher> pageTeacher = new Page<>(current, limit);
 
         //调用方法实现分页
         //调用方法时候，底层封装，把分页所有数据封装到pageTeacher对象里面
-        teacherService.page(pageTeacher,null);
+        teacherService.page(pageTeacher, null);
 
         long total = pageTeacher.getTotal();//总记录数
         List<EduTeacher> records = pageTeacher.getRecords(); //数据list集合
@@ -74,21 +75,21 @@ public class EduTeacherController {
 //        map.put("rows",records);
 //        return R.ok().data(map);
 
-        return R.ok().data("total",total).data("rows",records);
+        return R.ok().data("total", total).data("rows", records);
     }
 
     /*
-    * @RequestBody:使用json数据封装到对象的对象里面，需要使用post请求提交
-    * @ResponseBody:返回的是json数据
-    * @RequestBody(required = false)：表示的是参数值可以为空
-    * TeacherQuery teacherQuery：条件组合查询封装到对象中，再从对象中调用
-    * */
+     * @RequestBody:使用json数据封装到对象的对象里面，需要使用post请求提交
+     * @ResponseBody:返回的是json数据
+     * @RequestBody(required = false)：表示的是参数值可以为空
+     * TeacherQuery teacherQuery：条件组合查询封装到对象中，再从对象中调用
+     * */
     //4 条件查询带分页的方法
     @PostMapping("pageTeacherCondition/{current}/{limit}")
-    public R pageTeacherCondition(@PathVariable long current,@PathVariable long limit,
+    public R pageTeacherCondition(@PathVariable long current, @PathVariable long limit,
                                   @RequestBody(required = false) TeacherQuery teacherQuery) {
         //创建page对象
-        Page<EduTeacher> pageTeacher = new Page<>(current,limit);
+        Page<EduTeacher> pageTeacher = new Page<>(current, limit);
 
         //构建条件
         QueryWrapper<EduTeacher> wrapper = new QueryWrapper<>();
@@ -99,28 +100,57 @@ public class EduTeacherController {
         String begin = teacherQuery.getBegin();
         String end = teacherQuery.getEnd();
         //判断条件值是否为空，如果不为空拼接条件
-        if(!StringUtils.isEmpty(name)) {
+        if (!StringUtils.isEmpty(name)) {
             //构建条件
-            wrapper.like("name",name);
+            wrapper.like("name", name);
         }
-        if(!StringUtils.isEmpty(level)) {
-            wrapper.eq("level",level);
+        if (!StringUtils.isEmpty(level)) {
+            wrapper.eq("level", level);
         }
-        if(!StringUtils.isEmpty(begin)) {
+        if (!StringUtils.isEmpty(begin)) {
             //大于创建时间，因为写的是范围
-            wrapper.ge("gmt_create",begin);
+            wrapper.ge("gmt_create", begin);
         }
-        if(!StringUtils.isEmpty(end)) {
+        if (!StringUtils.isEmpty(end)) {
             //小于创建时间
-            wrapper.le("gmt_create",end);
+            wrapper.le("gmt_create", end);
         }
 
         //调用方法实现条件查询分页
-        teacherService.page(pageTeacher,wrapper);
+        teacherService.page(pageTeacher, wrapper);
 
         long total = pageTeacher.getTotal();//总记录数
         List<EduTeacher> records = pageTeacher.getRecords(); //数据list集合
-        return R.ok().data("total",total).data("rows",records);
+        return R.ok().data("total", total).data("rows", records);
+    }
+
+    //添加讲师接口的方法
+    @PostMapping("addTeacher")
+    public R addTeacher(@RequestBody EduTeacher eduTeacher) {
+        boolean save = teacherService.save(eduTeacher);
+        if (save) {
+            return R.ok();
+        } else {
+            return R.error();
+        }
+    }
+
+    //根据讲师id进行查询
+    @GetMapping("getTeacher/{id}")
+    public R getTeacher(@PathVariable String id) {
+        EduTeacher eduTeacher = teacherService.getById(id);
+        return R.ok().data("teacher", eduTeacher);
+    }
+
+    //讲师修改功能
+    @PostMapping("updateTeacher")
+    public R updateTeacher(@RequestBody EduTeacher eduTeacher) {
+        boolean flag = teacherService.updateById(eduTeacher);
+        if (flag) {
+            return R.ok();
+        } else {
+            return R.error();
+        }
     }
 
 }
