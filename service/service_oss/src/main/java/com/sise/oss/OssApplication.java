77b473a245d17535882a747b8cc42c578082d7c9
myth-git <1610680426@qq.com>
2022-03-12 18:38:35 +0800

    oos上传讲师头像

---
+++ service/service_oss/src/main/java/com/sise/oss/OssApplication.java
@@ -0,0 +1,15 @@
+package com.sise.oss;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+//参数：表示默认不去加载数据库的配置
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
+@ComponentScan(basePackages = {"com.sise"})
+public class OssApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OssApplication.class, args);
+    }
+}
---
+++ service/service_oss/src/main/java/com/sise/oss/controller/OssController.java
@@ -0,0 +1,29 @@
+package com.sise.oss.controller;
+
+import com.sise.commonutils.R;
+import com.sise.oss.service.OssService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+@RestController
+@RequestMapping("/eduoss/fileoss")
+@CrossOrigin//解决跨域问题
+public class OssController {
+
+    @Autowired
+    private OssService ossService;
+
+    //上传头像的方法
+    @PostMapping
+    public R uploadOssFile(MultipartFile file) {
+        //获取上传文件  MultipartFile
+        //返回上传到oss的路径
+        String url = ossService.uploadFileAvatar(file);
+        return R.ok().data("url", url);
+    }
+}
