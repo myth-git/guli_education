101cbe308554656d3f042a6dbb2a5de7a7eafc0a
myth-git <1610680426@qq.com>
2022-03-26 17:01:04 +0800

    课程大纲列表

--- service/service_edu/src/main/java/com/sise/eduservice/controller/EduChapterController.java
+++ service/service_edu/src/main/java/com/sise/eduservice/controller/EduChapterController.java
@@ -1,9 +1,14 @@
 package com.sise.eduservice.controller;
 
 
-import org.springframework.web.bind.annotation.RequestMapping;
+import com.sise.commonutils.R;
+import com.sise.eduservice.entity.chapter.ChapterVo;
+import com.sise.eduservice.service.EduChapterService;
+import io.swagger.annotations.Authorization;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
-import org.springframework.web.bind.annotation.RestController;
+import java.util.List;
 
 /**
  * <p>
@@ -14,8 +19,19 @@ import org.springframework.web.bind.annotation.RestController;
  * @since 2022-03-25
  */
 @RestController
-@RequestMapping("/eduservice/edu-chapter")
+@RequestMapping("/eduservice/chapter")
+@CrossOrigin
 public class EduChapterController {
+
+    @Autowired
+    private EduChapterService chapterService;
+
+    //课程大纲列表,根据课程id进行查询
+    @GetMapping("getChapterVideo/{courseId}")
+    public R getChapterVideo(@PathVariable String courseId) {
+        List<ChapterVo> list = chapterService.getChapterVideoByCourseId(courseId);
+        return R.ok().data("allChapterVideo", list);
+    }
 
 }
 
--- service/service_edu/src/main/java/com/sise/eduservice/service/EduChapterService.java
+++ service/service_edu/src/main/java/com/sise/eduservice/service/EduChapterService.java
@@ -2,6 +2,9 @@ package com.sise.eduservice.service;
 
 import com.sise.eduservice.entity.EduChapter;
 import com.baomidou.mybatisplus.extension.service.IService;
+import com.sise.eduservice.entity.chapter.ChapterVo;
+
+import java.util.List;
 
 /**
  * <p>
@@ -12,5 +15,6 @@ import com.baomidou.mybatisplus.extension.service.IService;
  * @since 2022-03-25
  */
 public interface EduChapterService extends IService<EduChapter> {
-
+    //课程大纲列表,根据课程id进行查询
+    List<ChapterVo> getChapterVideoByCourseId(String courseId);
 }
