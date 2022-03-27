7f6c880212b8e830b3c1a2d3ecd3d4bbf258809a
myth-git <1610680426@qq.com>
2022-03-27 11:44:12 +0800

    课程信息中章节增删改查

--- service/service_edu/src/main/java/com/sise/eduservice/controller/EduChapterController.java
+++ service/service_edu/src/main/java/com/sise/eduservice/controller/EduChapterController.java
@@ -2,6 +2,7 @@ package com.sise.eduservice.controller;
 
 
 import com.sise.commonutils.R;
+import com.sise.eduservice.entity.EduChapter;
 import com.sise.eduservice.entity.chapter.ChapterVo;
 import com.sise.eduservice.service.EduChapterService;
 import io.swagger.annotations.Authorization;
@@ -31,6 +32,37 @@ public class EduChapterController {
     public R getChapterVideo(@PathVariable String courseId) {
         List<ChapterVo> list = chapterService.getChapterVideoByCourseId(courseId);
         return R.ok().data("allChapterVideo", list);
+    }
+
+    //添加章节
+    @PostMapping("addChapter")
+    public R addChapter(@RequestBody EduChapter eduChapter) {
+        chapterService.save(eduChapter);
+        return R.ok();
+    }
+
+    //根据id查询章节
+    @GetMapping("getChapterInfo/{chapterId}")
+    public R getChapterInfo(@PathVariable String chapterId){
+        EduChapter eduChapter = chapterService.getById(chapterId);
+        return R.ok().data("chapter",eduChapter);
+    }
+    //修改章节
+    @PostMapping("updateChapter")
+    public R updateChapter(@RequestBody EduChapter eduChapter){
+        chapterService.updateById(eduChapter);
+        return R.ok();
+    }
+    //删除章节
+    @DeleteMapping("{chapterId}")
+    public R deleteChapter(@PathVariable String chapterId){
+        boolean flag = chapterService.deleteChapter(chapterId);
+
+        if (flag){
+            return R.ok();
+        }else {
+            return R.error();
+        }
     }
 
 }
--- service/service_edu/src/main/java/com/sise/eduservice/service/impl/EduChapterServiceImpl.java
+++ service/service_edu/src/main/java/com/sise/eduservice/service/impl/EduChapterServiceImpl.java
@@ -9,6 +9,7 @@ import com.sise.eduservice.mapper.EduChapterMapper;
 import com.sise.eduservice.service.EduChapterService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.sise.eduservice.service.EduVideoService;
+import com.sise.servicebase.exceptionhandler.GuliException;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -29,13 +30,13 @@ public class EduChapterServiceImpl extends ServiceImpl<EduChapterMapper, EduChap
 
     @Autowired
     private EduVideoService eduVideoService;//注入小节service
-    
+
     //课程大纲列表,根据课程id进行查询
     @Override
     public List<ChapterVo> getChapterVideoByCourseId(String courseId) {
         //1 根据课程id查询课程里面所有的章节
         QueryWrapper<EduChapter> wrapperChapter = new QueryWrapper<>();
-        wrapperChapter.eq("course_id",courseId);
+        wrapperChapter.eq("course_id", courseId);
         List<EduChapter> eduChapterList = baseMapper.selectList(wrapperChapter);
         //2 根据课程id查询课程里面所有的小节
         QueryWrapper<EduVideo> wrapperVideo = new QueryWrapper<>();
@@ -52,7 +53,7 @@ public class EduChapterServiceImpl extends ServiceImpl<EduChapterMapper, EduChap
             EduChapter eduChapter = eduChapterList.get(i);
             //eduChapter对象值复制到ChapterVo里面
             ChapterVo chapterVo = new ChapterVo();
-            BeanUtils.copyProperties(eduChapter,chapterVo);
+            BeanUtils.copyProperties(eduChapter, chapterVo);
             //把chapterVo放到最终list集合
             finalList.add(chapterVo);
 
@@ -64,10 +65,10 @@ public class EduChapterServiceImpl extends ServiceImpl<EduChapterMapper, EduChap
                 //得到每个小节
                 EduVideo eduVideo = eduVideoList.get(m);
                 //判断：小节里面chapterid和章节里面id是否一样
-                if(eduVideo.getChapterId().equals(eduChapter.getId())) {
+                if (eduVideo.getChapterId().equals(eduChapter.getId())) {
                     //进行封装
                     VideoVo videoVo = new VideoVo();
-                    BeanUtils.copyProperties(eduVideo,videoVo);
+                    BeanUtils.copyProperties(eduVideo, videoVo);
                     //放到小节封装集合
                     videoList.add(videoVo);
                 }
@@ -77,5 +78,22 @@ public class EduChapterServiceImpl extends ServiceImpl<EduChapterMapper, EduChap
         }
 
         return finalList;
+    }
+
+    //删除章节
+    @Override
+    public boolean deleteChapter(String chapterId) {
+        //根据chapterId查询小节，如果查询出数据，不进行删除
+        QueryWrapper<EduVideo> wrapper = new QueryWrapper<>();
+        wrapper.eq("chapter_id", chapterId);
+        int count = eduVideoService.count(wrapper);
+        if (count > 0) {//查询出小节。不进行删除
+            throw new GuliException(20001, "不能删除");
+        } else {
+            //删除章节
+            int result = baseMapper.deleteById(chapterId);
+            return result > 0;
+        }
+
     }
 }
