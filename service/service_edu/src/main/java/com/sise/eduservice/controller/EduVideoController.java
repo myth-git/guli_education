28c9fae8d6b33ed231a29fa63e0250b4bf019f23
myth-git <1610680426@qq.com>
2022-03-27 15:52:01 +0800

    课程信息中小节增删改查

--- service/service_edu/src/main/java/com/sise/eduservice/controller/EduVideoController.java
+++ service/service_edu/src/main/java/com/sise/eduservice/controller/EduVideoController.java
@@ -1,9 +1,13 @@
 package com.sise.eduservice.controller;
 
 
-import org.springframework.web.bind.annotation.RequestMapping;
+import com.sise.commonutils.R;
+import com.sise.eduservice.entity.EduVideo;
+import com.sise.eduservice.service.EduVideoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 /**
  * <p>
@@ -14,8 +18,43 @@ import org.springframework.web.bind.annotation.RestController;
  * @since 2022-03-25
  */
 @RestController
-@RequestMapping("/eduservice/edu-video")
+@RequestMapping("/eduservice/video")
+@CrossOrigin
 public class EduVideoController {
+
+    @Autowired
+    private EduVideoService videoService;
+
+    //增加小节
+    @PostMapping("addVideo")
+    public R addVideo(@RequestBody EduVideo eduVideo){
+        videoService.save(eduVideo);
+        return R.ok();
+    }
+    //删除小节
+    //TODO 后面要完善
+    @DeleteMapping("{id}")
+    public R deleteVideo(@PathVariable String id){
+        videoService.removeById(id);
+        return R.ok();
+    }
+
+    //TODO 修改小节
+    //根据id查询小节
+    @GetMapping("getVideoInfo/{id}")
+    public R getVideoInfo(@PathVariable String id){
+        EduVideo eduVideo = videoService.getById(id);
+        return R.ok().data("video",eduVideo);
+    }
+
+    //修改小节
+    @PostMapping("updateVideo")
+    public R updateVideo(@RequestBody EduVideo eduVideo){
+        videoService.updateById(eduVideo);
+        return R.ok();
+    }
+
+
 
 }
 
