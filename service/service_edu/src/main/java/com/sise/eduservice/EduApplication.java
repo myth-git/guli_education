400845f8bce2da759c90cfd5013a2804fdab42db
myth-git <1610680426@qq.com>
2022-04-04 14:52:46 +0800

    edu、vod注册nacos服务中心

--- service/service_edu/src/main/java/com/sise/eduservice/EduApplication.java
+++ service/service_edu/src/main/java/com/sise/eduservice/EduApplication.java
@@ -2,9 +2,11 @@ package com.sise.eduservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@EnableDiscoveryClient  //nacos注册
 @ComponentScan(basePackages = {"com.sise"})//主要扫描swagger配置类。只会默认扫描项目包的内容
 public class EduApplication {
     public static void main(String[] args) {
