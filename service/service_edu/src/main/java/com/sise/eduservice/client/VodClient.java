43a29361a384d71e3ac5f51d0f6cc10440a3e171
myth-git <1610680426@qq.com>
2022-04-04 16:21:34 +0800

    edu远程调用根据课程id删除视频

--- service/service_edu/src/main/java/com/sise/eduservice/client/VodClient.java
+++ service/service_edu/src/main/java/com/sise/eduservice/client/VodClient.java
@@ -5,6 +5,9 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
 
 @FeignClient("service-vod")
 @Component
@@ -14,4 +17,10 @@ public interface VodClient {
     //@PathVariable注解一定要指定参数名称，否则出错
     @DeleteMapping("/eduvod/video/removeAlyVideo/{id}")
     public R removeAlyVideo(@PathVariable("id") String id);
+
+    //定义调用删除多个视频的方法
+    //删除多个阿里云视频的方法
+    //参数多个视频id  List videoIdList
+    @DeleteMapping("/eduvod/video/delete-batch")
+    public R deleteBatch(@RequestParam("videoIdList") List<String> videoIdList);
 }
