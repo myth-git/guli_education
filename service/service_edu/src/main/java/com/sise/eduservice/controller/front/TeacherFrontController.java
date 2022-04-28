16104fb370c727d6e998c11c47dbefd33a345b7f
myth-git <1610680426@qq.com>
2022-04-28 18:03:58 +0800

    前台讲师详情接口

--- service/service_edu/src/main/java/com/sise/eduservice/controller/front/TeacherFrontController.java
+++ service/service_edu/src/main/java/com/sise/eduservice/controller/front/TeacherFrontController.java
@@ -1,12 +1,16 @@
 package com.sise.eduservice.controller.front;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.sise.commonutils.R;
+import com.sise.eduservice.entity.EduCourse;
 import com.sise.eduservice.entity.EduTeacher;
+import com.sise.eduservice.service.EduCourseService;
 import com.sise.eduservice.service.EduTeacherService;
 import org.springframework.beans.factory.annotation.Autowired;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.Map;
 
 @RestController
@@ -15,6 +19,8 @@ import java.util.Map;
 public class TeacherFrontController {
     @Autowired
     private EduTeacherService teacherService;
+    @Autowired
+    private EduCourseService courseService;
 
     //1 分页查询讲师的方法
     @PostMapping("getTeacherFrontList/{page}/{limit}")
@@ -23,5 +29,18 @@ public class TeacherFrontController {
         Map<String,Object> map = teacherService.getTeacherFrontList(pageTeacher);
         //返回分页所有数据
         return R.ok().data(map);
+    }
+
+    //2 讲师详情的功能
+    @GetMapping("getTeacherFrontInfo/{teacherId}")
+    public R getTeacherFrontInfo(@PathVariable String teacherId) {
+        //1 根据讲师id查询讲师基本信息
+        EduTeacher eduTeacher = teacherService.getById(teacherId);
+        //2 根据讲师id查询所讲课程
+        QueryWrapper<EduCourse> wrapper = new QueryWrapper<>();
+        wrapper.eq("teacher_id",teacherId);
+        List<EduCourse> courseList = courseService.list(wrapper);
+        return R.ok().data("teacher",eduTeacher).data("courseList",courseList);
+
     }
 }
