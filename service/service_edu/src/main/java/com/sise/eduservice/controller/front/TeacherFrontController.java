93826d5fec2c93118f090cd62897ed2dff5d7d14
myth-git <1610680426@qq.com>
2022-04-28 09:54:39 +0800

    前台讲师分页查询接口

---
+++ service/service_edu/src/main/java/com/sise/eduservice/controller/front/TeacherFrontController.java
@@ -0,0 +1,27 @@
+package com.sise.eduservice.controller.front;
+
+import com.sise.commonutils.R;
+import com.sise.eduservice.entity.EduTeacher;
+import com.sise.eduservice.service.EduTeacherService;
+import org.springframework.beans.factory.annotation.Autowired;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("/eduservice/teacherfront")
+@CrossOrigin
+public class TeacherFrontController {
+    @Autowired
+    private EduTeacherService teacherService;
+
+    //1 分页查询讲师的方法
+    @PostMapping("getTeacherFrontList/{page}/{limit}")
+    public R getTeacherFrontList(@PathVariable long page, @PathVariable long limit) {
+        Page<EduTeacher> pageTeacher = new Page<>(page,limit);
+        Map<String,Object> map = teacherService.getTeacherFrontList(pageTeacher);
+        //返回分页所有数据
+        return R.ok().data(map);
+    }
+}
--- service/service_edu/src/main/java/com/sise/eduservice/service/EduTeacherService.java
+++ service/service_edu/src/main/java/com/sise/eduservice/service/EduTeacherService.java
@@ -1,7 +1,10 @@
 package com.sise.eduservice.service;
 
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.sise.eduservice.entity.EduTeacher;
 import com.baomidou.mybatisplus.extension.service.IService;
+
+import java.util.Map;
 
 /**
  * <p>
@@ -12,5 +15,6 @@ import com.baomidou.mybatisplus.extension.service.IService;
  * @since 2022-03-08
  */
 public interface EduTeacherService extends IService<EduTeacher> {
-
+    //1 分页查询讲师的方法
+    Map<String, Object> getTeacherFrontList(Page<EduTeacher> pageTeacher);
 }
