efbb285e4122dcb776e5936c1729eeeb421ced60
myth-git <1610680426@qq.com>
2022-04-30 15:19:40 +0800

    生成支付二维码和查询订单状态接口

--- service/service_order/src/main/java/com/sise/eduorder/controller/PayLogController.java
+++ service/service_order/src/main/java/com/sise/eduorder/controller/PayLogController.java
@@ -1,9 +1,12 @@
 package com.sise.eduorder.controller;
 
 
-import org.springframework.web.bind.annotation.RequestMapping;
+import com.sise.commonutils.R;
+import com.sise.eduorder.service.PayLogService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
-import org.springframework.web.bind.annotation.RestController;
+import java.util.Map;
 
 /**
  * <p>
@@ -14,8 +17,41 @@ import org.springframework.web.bind.annotation.RestController;
  * @since 2022-04-29
  */
 @RestController
-@RequestMapping("/eduorder/pay-log")
+@RequestMapping("/eduorder/paylog")
+@CrossOrigin
 public class PayLogController {
+
+    @Autowired
+    private PayLogService payLogService;
+
+    //生成微信支付二维码接口
+    //参数是订单号
+    @GetMapping("createNative/{orderNo}")
+    public R createNative(@PathVariable String orderNo) {
+        //返回信息，包含二维码地址，还有其他需要的信息
+        Map map = payLogService.createNatvie(orderNo);
+        System.out.println("****返回二维码map集合:"+map);
+        return R.ok().data(map);
+    }
+
+    //查询订单支付状态
+    //参数：订单号，根据订单号查询 支付状态
+    @GetMapping("queryPayStatus/{orderNo}")
+    public R queryPayStatus(@PathVariable String orderNo) {
+        Map<String,String> map = payLogService.queryPayStatus(orderNo);
+        System.out.println("*****查询订单状态map集合:"+map);
+        if (map == null){
+            return R.error().message("支付出错了");
+        }
+        //如果返回map里面不为空，通过map获取订单状态 trade_state微信支付固定的字段
+        if (map.get("trade_state").equals("SUCCESS")){//支付成功
+            //添加记录到支付表，更新订单表订单状态
+            payLogService.updateOrdersStatus(map);
+            return R.ok().message("支付成功");
+        }
+        return R.ok().code(25000).message("支付中");
+
+    }
 
 }
 
--- service/service_order/src/main/java/com/sise/eduorder/service/PayLogService.java
+++ service/service_order/src/main/java/com/sise/eduorder/service/PayLogService.java
@@ -3,6 +3,8 @@ package com.sise.eduorder.service;
 import com.sise.eduorder.entity.PayLog;
 import com.baomidou.mybatisplus.extension.service.IService;
 
+import java.util.Map;
+
 /**
  * <p>
  * 支付日志表 服务类
@@ -12,5 +14,10 @@ import com.baomidou.mybatisplus.extension.service.IService;
  * @since 2022-04-29
  */
 public interface PayLogService extends IService<PayLog> {
-
+    //生成微信支付二维码接口
+    Map createNatvie(String orderNo);
+    //查询订单支付状态
+    Map<String, String> queryPayStatus(String orderNo);
+    //添加支付记录和更新订单状态
+    void updateOrdersStatus(Map<String, String> map);
 }
