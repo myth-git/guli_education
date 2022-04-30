5e1f5dfcdbb2ba24dd1f551c97176f73fc8facbe
myth-git <1610680426@qq.com>
2022-04-30 22:20:54 +0800

    整合springboot定时任务

--- service/service_statistics/src/main/java/com/sise/staservice/StaApplication.java
+++ service/service_statistics/src/main/java/com/sise/staservice/StaApplication.java
@@ -6,12 +6,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @ComponentScan(basePackages = {"com.sise"})
 @EnableDiscoveryClient
 @EnableFeignClients
 @MapperScan("com.sise.staservice.mapper")
+@EnableScheduling//定时任务注解
 public class StaApplication {
 
     public static void main(String[] args) {
