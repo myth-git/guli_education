70427212faae87d8ed2cab7e8bcd71ed0374a572
myth-git <1610680426@qq.com>
2022-05-01 12:46:34 +0800

    数据折线统计后台接口

--- service/service_statistics/src/main/java/com/sise/staservice/service/StatisticsDailyService.java
+++ service/service_statistics/src/main/java/com/sise/staservice/service/StatisticsDailyService.java
@@ -3,6 +3,8 @@ package com.sise.staservice.service;
         import com.sise.staservice.entity.StatisticsDaily;
         import com.baomidou.mybatisplus.extension.service.IService;
 
+        import java.util.Map;
+
 /**
  * <p>
  * 网站统计日数据 服务类
@@ -14,4 +16,6 @@ package com.sise.staservice.service;
 public interface StatisticsDailyService extends IService<StatisticsDaily> {
     //统计某一天注册人数,生成统计数据
     void registerCount(String day);
+    //图表显示，返回两部分数据，日期json数组，数量json数组
+    Map<String, Object> getShowData(String type, String begin, String end);
 }
